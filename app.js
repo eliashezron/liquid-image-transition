b7ab38a844fad5fd934f892e25b8794e3b744f3a
elias-hezron <eliashezron23.com>
2021-01-08 21:26:02 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.2,
+    image1: './images/stil-flRm0z3MEoA-unsplash.jpg',
+    image2:'./images/minh-pham-lB9ylP8e9Sg-unsplash.jpg',
+    // angle : Math.PI/8,
+    displacementImage: './images/heightMap.png'
+})
